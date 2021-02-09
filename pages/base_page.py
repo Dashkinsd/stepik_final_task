86077907a46d659600b6d4fd9a2b583e5b12a21d
Dashkinsd <dar.khlynina@yandex.ru>
2021-02-09 14:48:19 +0300

    Модуль 4

---
+++ pages/base_page.py
@@ -0,0 +1,7 @@
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = webdriver.Chrome()
+        self.url = http://selenium1py.pythonanywhere.com/
+    
+    def open(self):
+        self.browser.get(self.url)
