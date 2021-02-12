003fe0a87b9bdc3b57b2434f31fd1ebf7466dd92
Dashkinsd <dar.khlynina@yandex.ru>
2021-02-12 18:04:41 +0300

    Поиск бага, с параметром

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,30 @@
 from .pages.product_page import ProductPage
+import pytest
 
-#Нажимаем на кнопку Корзины
-def test_guest_click_button_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+@pytest.mark.parametrize('links', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])                                                             
+def test_guest_click_button_basket(browser, links): #Нажимаем на кнопку Корзины
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}"
     page = ProductPage(browser, link)
     page.open()
     page.click_button_basket()
+    print (browser.current_url)
+
+@pytest.mark.parametrize('links', [*range(0,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_book_title_match(browser, links):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.book_title_match()
+    print (browser.current_url)
     
+    
+@pytest.mark.parametrize('links', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_price_book_match(browser, links):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.price_book_match()
+    print (browser.current_url)
+    
+#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
