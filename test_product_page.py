cf9b0dc7be64bc799c1ed0f7aad72a7207051ca3
Dashkinsd <dar.khlynina@yandex.ru>
2021-02-15 17:18:30 +0300

    гость может перейти на страницу логина со страницы Х

--- test_product_page.py
+++ test_product_page.py
@@ -28,3 +28,42 @@ def test_guest_price_book_match(browser, links):
     print (browser.current_url)
     
 #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+    print (browser.current_url)
+    
+def test_guest_cant_see_success_message(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.assert_should_not_be_success_message()
+    print (browser.current_url)
+
+@pytest.mark.xfail    
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.explicit_should_not_be_success_message()
+    print (browser.current_url) 
+    
+#гость может перейти на страницу логина со страницы Х    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+#гость может перейти на страницу логина со страницы продукта
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_login_page()
+
+    
