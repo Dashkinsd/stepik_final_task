397f0f1494749a675054309488a1d0fe437069e4
Dashkinsd <dar.khlynina@yandex.ru>
2021-02-25 18:59:53 +0300

    наследование и отрицательные проверки

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
+import time 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    page = MainPage(browser, link)                        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                                           # открываем страницу   
+    page.go_to_login_page()                               # выполняем метод страницы — переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()          
     
@@ -33,3 +36,15 @@ def test_guest_can_see_register_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+    
+    #РАБОТАЕТ ИЗ BasePage! 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link) #см. Base_Page def __init__
+    page.open() # переходим по ссылке
+    page.go_to_basket()
+    #time.sleep(5)
+    #должен обратиться к методу basket_empty_message на странице basket_page
+    page.basket_empty_message()
+    #time.sleep(5)
+    page.basket_without_product()
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 
@@ -59,11 +60,17 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
     
-#гость может перейти на страницу логина со страницы продукта
+гость может перейти на страницу логина со страницы продукта
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.click_login_page()
 
-    
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.open_product_page()
+    page.go_to_basket()
+
